a1755a8e2bc80722b0860da914e4da1ff557b95f
vasa3331 <vasiliymartinov@gmail.com>
2023-05-08 21:10:23 +0300

    feat: finish 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const { mkdir } = require('node:fs/promises');
+const { copyFile, constants } = require('node:fs/promises');
+
+async function makeDirectory() {
+    const projectFolder = path.join(__dirname, 'files-copy');
+    const filesCopy = fs.mkdir(projectFolder, { recursive: true }, err => {
+        if(err) throw err;
+    });
+    return filesCopy;
+}
+  
+makeDirectory();
+
+fs.readdir(path.join(__dirname, '/files'), (err, files) => {
+    if(err) throw err; // не прочитать содержимое папки
+    for(let file of files) {
+        fs.copyFile(path.join(__dirname, '/files', file), path.join(__dirname, 'files-copy', file), err => {
+            if(err) throw err; // не удалось скопировать файл
+        });
+    }
+ });
+
+
 
